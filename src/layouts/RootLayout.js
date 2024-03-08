d2ee55472d60342db82ae99e39a85033bf7b1b66
Yuzbekov Elvin <yuzbekov79@gmail.com>
2024-03-08 22:20:02 +0400

    change

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -5,7 +5,6 @@ import Footer from '../components/Footer'
 import Whatsapp from '../components/fixedButtons/Whatsapp'
 import ScrollTop from '../components/fixedButtons/ScrollTop'
 import ScrollToTop from '../components/scroll/ScrollToTop'
-import CircularNavigation from '../components/fixedButtons/CircularNavigation'
 
 function RootLayout() {
   return (
@@ -15,7 +14,6 @@ function RootLayout() {
         <main>
             <Outlet/>
             <Whatsapp/>
-            {/* <CircularNavigation/> */}
             <ScrollTop/>
         </main>
         <Footer/>
